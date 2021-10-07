81511f5c05e1acefc83752f1acf3bee9d7a76588
ArrththiT <91230192+ArrththiT@users.noreply.github.com>
2021-10-06 20:33:50 -0400

    Update CardTrick.java

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/Friday/CardTrick.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/Friday/CardTrick.java
@@ -9,7 +9,10 @@ package ca.sheridancollege.week3.softwarefundamentals.ice1.Friday;
 public class CardTrick {
     public static void main(String[] args)           
     {
-    Card card1 = new Card();   
+    Card card1 = new Card();
+    System.out.println(card1.getSuit);
+    System.out.println(card1.getValue);
+    
     }
     
 }
